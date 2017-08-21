4aab7fcc457ae44651d90e82ed7546e23856c640
briankeane <lonesomewhistle@gmail.com>
2017-08-20 20:03:19 -0500

    replace target with ios and osx specific targets.  Configure schemes for shared and to gather coverage data

---
+++ SwiftRemoteFileCache/SwiftRemoteFileCacheIOS/SwiftRemoteFileCacheIOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftRemoteFileCacheIOS.h
+//  SwiftRemoteFileCacheIOS
+//
+//  Created by Brian D Keane on 8/20/17.
+//  Copyright © 2017 Brian D Keane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftRemoteFileCacheIOS.
+FOUNDATION_EXPORT double SwiftRemoteFileCacheIOSVersionNumber;
+
+//! Project version string for SwiftRemoteFileCacheIOS.
+FOUNDATION_EXPORT const unsigned char SwiftRemoteFileCacheIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRemoteFileCacheIOS/PublicHeader.h>
+
+
---
+++ SwiftRemoteFileCache/SwiftRemoteFileCacheOSX/SwiftRemoteFileCacheOSX.h
@@ -0,0 +1,19 @@
+//
+//  SwiftRemoteFileCacheOSX.h
+//  SwiftRemoteFileCacheOSX
+//
+//  Created by Brian D Keane on 8/20/17.
+//  Copyright © 2017 Brian D Keane. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftRemoteFileCacheOSX.
+FOUNDATION_EXPORT double SwiftRemoteFileCacheOSXVersionNumber;
+
+//! Project version string for SwiftRemoteFileCacheOSX.
+FOUNDATION_EXPORT const unsigned char SwiftRemoteFileCacheOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRemoteFileCacheOSX/PublicHeader.h>
+
+
